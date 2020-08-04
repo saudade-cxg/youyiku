d1ef6201b6d47587774f7bef0cfc2b50ae1f4826
saudade-cxg <cxg437022665@163.com>
2020-08-04 20:00:44 +0800

    列表页第一次渲染分页排序

--- script/main.js
+++ script/main.js
@@ -1,28 +1,35 @@
 require.config({
-    paths: {
-        'jquery': 'https://cdn.bootcdn.net/ajax/libs/jquery/1.12.4/jquery.min',
-        'lazyload':'https://cdn.bootcdn.net/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min'
-    },
-    shim:{
-        'lazyload':['jquery']
-    }
+  paths: {
+    'jquery': 'https://cdn.bootcdn.net/ajax/libs/jquery/1.12.4/jquery.min',
+    'lazyload': 'https://cdn.bootcdn.net/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min',
+    'pagination': 'http://localhost/homework-JS/youyiku/src/script/jquery.pagination.js'
+  },
+  shim: {
+    'lazyload': ['jquery'],
+    'jquery.pagination': ['jquery']
+  }
 });
 
-require(['jquery','lazyload'], function ($) {
-    require(['public'],function(public){
-        public.navSuspension();
-        public.toTop();
-        public.stairs();
-        public.navMenu();
-    })
-    let mod = $('#currentpage').attr('currentmod');
-    if (mod) {
-        require([mod], function (modlist) {
-           modlist.noticeBanner();
-           modlist.goodsBanner();
-           modlist.renderWomen();
-           modlist.renderMan();
-        //    modlist.renderList();
-        });
-    }
+require(['jquery', 'lazyload', 'jquery.pagination'], function ($) {
+  require(['public'], function (public) {
+    public.navSuspension();
+    public.toTop();
+    public.stairs();
+    public.navMenu();
+  })
+  let mod = $('#currentpage').attr('currentmod');
+  if (mod == 'modfirst') {
+    require([mod], function (modlist) {
+      modlist.noticeBanner();
+      modlist.goodsBanner();
+      modlist.renderWomen();
+      modlist.renderMan();
+    });
+  } else if (mod == 'modlist') {
+    require([mod], function (modlist) {
+      // modlist.renderList();
+      // modlist.renderPage();
+      modlist.renderSort();
+    });
+  }
 });
--- script/modlist.js
+++ script/modlist.js
@@ -1,8 +1,327 @@
 define([], function () {
   return {
-    // 列表页的渲染：
+    // 1.列表页的渲染：
     renderList: function () {
+      let $goodsList = $('.wrap')
+      $.ajax({
+        url: 'http://localhost/homework-JS/youyiku/php/listdata.php',
+        dataType: 'json'
+      }).done(function (data) {
+        let strhtml = '<ul>';
+        $.each(data, function (index, value) {
+          strhtml += `
+          <li>
+            <div class="logo">
+              <img src="${value.active}" alt="">
+            </div>
+            <div class="pic">
+              <a href="details.html">
+              <img data-original="${value.url}" class="lazy" width="212" height="222"/></a>
+            </div>
+            <ol>
+              <li>
+                <img src="${value.color1}" alt="">
+              </li>
+              <li>
+                <img src="${value.color2}" alt="">
+              </li>
+              <li>
+                <img src="${value.color3}" alt="">
+              </li>
+              <li>
+                <img src="${value.color4}" alt="">
+              </li>
+              <li>
+                <img src="${value.color5}" alt="">
+              </li>
+              <li>
+                <img src="${value.color6}" alt="">
+              </li>
+            </ol>
+            <p class="type">${value.title1}<span class="size">${value.size}</span></p>
+            <p class="clothes">${value.title2}</p>
+            <p class="price">${value.price}</p>
+            <div class="star">
+              <span class="iconfont icon-wujiaoxing"></span>
+              <span class="iconfont icon-wujiaoxing"></span>
+              <span class="iconfont icon-wujiaoxing"></span>
+              <span class="iconfont icon-wujiaoxing"></span>
+              <span class="iconfont icon-wujiaoxing"></span>
+              <span>(${value.sailnumber})</span>
+            </div>
+          </li>
+          `
+        });
+        // 2.懒加载
+        strhtml += '</ul>';
+        $goodsList.html(strhtml);
+        $(function () {
+          $("img.lazy").lazyload({ effect: "fadeIn" });
+        });
+      })
+    },
+    // 3.分页：
+    renderPage: function () {
+      let $goodsList = $('.wrap');
+      $('.page').pagination({
+        pageCount: 3,
+        jump: true,
+        prevContent: '上一页',
+        nextContent: '下一页',
+        callback: function (api) {
+          // console.log(api.getCurrent());
+          $.ajax({
+            url: 'http://localhost/homework-JS/youyiku/php/listdata.php',
+            data: {
+              page: api.getCurrent()
+            },
+            dataType: 'json'
+          }).done(function (data) {
+            let strhtml = '<ul>';
+            $.each(data, function (index, value) {
+              strhtml += `
+              <li>
+                <div class="logo">
+                  <img src="${value.active}" alt="">
+                </div>
+                <div class="pic">
+                  <a href="details.html">
+                  <img data-original="${value.url}" class="lazy" width="212" height="222"/></a>
+                </div>
+                <ol>
+                  <li>
+                    <img src="${value.color1}" alt="">
+                  </li>
+                  <li>
+                    <img src="${value.color2}" alt="">
+                  </li>
+                  <li>
+                    <img src="${value.color3}" alt="">
+                  </li>
+                  <li>
+                    <img src="${value.color4}" alt="">
+                  </li>
+                  <li>
+                    <img src="${value.color5}" alt="">
+                  </li>
+                  <li>
+                    <img src="${value.color6}" alt="">
+                  </li>
+                </ol>
+                <p class="type">${value.title1}<span class="size">${value.size}</span></p>
+                <p class="clothes">${value.title2}</p>
+                <p class="price">${value.price}</p>
+                <div class="star">
+                  <span class="iconfont icon-wujiaoxing"></span>
+                  <span class="iconfont icon-wujiaoxing"></span>
+                  <span class="iconfont icon-wujiaoxing"></span>
+                  <span class="iconfont icon-wujiaoxing"></span>
+                  <span class="iconfont icon-wujiaoxing"></span>
+                  <span>(${value.sailnumber})</span>
+                </div>
+              </li>
+              `
+            });
+            // 2.懒加载
+            strhtml += '</ul>';
+            $goodsList.html(strhtml);
+            $(function () {
+              $("img.lazy").lazyload({ effect: "fadeIn" });
+            });
+          })
+        }
+      })
+    },
+    // 4.默认排序：
+    renderSort: function () {
+      let array_default = [];
+      let array = [];
+      let prev = null;
+      let next = null;
+      let $btn = $('.rank button');
+      let $goodsList = $('.wrap')
+      $.ajax({
+        url: 'http://localhost/homework-JS/youyiku/php/listdata.php',
+        dataType: 'json'
+      }).done(function (data) {
+        let strhtml = '<ul>';
+        $.each(data, function (index, value) {
+          strhtml += `
+            <li>
+              <div class="logo">
+                <img src="${value.active}" alt="">
+              </div>
+              <div class="pic">
+                <a href="details.html">
+                <img data-original="${value.url}" class="lazy" width="212" height="222"/></a>
+              </div>
+              <ol>
+                <li>
+                  <img src="${value.color1}" alt="">
+                </li>
+                <li>
+                  <img src="${value.color2}" alt="">
+                </li>
+                <li>
+                  <img src="${value.color3}" alt="">
+                </li>
+                <li>
+                  <img src="${value.color4}" alt="">
+                </li>
+                <li>
+                  <img src="${value.color5}" alt="">
+                </li>
+                <li>
+                  <img src="${value.color6}" alt="">
+                </li>
+              </ol>
+              <p class="type">${value.title1}<span class="size">${value.size}</span></p>
+              <p class="clothes">${value.title2}</p>
+              <p class="price">${value.price}</p>
+              <div class="star">
+                <span class="iconfont icon-wujiaoxing"></span>
+                <span class="iconfont icon-wujiaoxing"></span>
+                <span class="iconfont icon-wujiaoxing"></span>
+                <span class="iconfont icon-wujiaoxing"></span>
+                <span class="iconfont icon-wujiaoxing"></span>
+                <span>(${value.sailnumber})</span>
+              </div>
+            </li>
+            `
+        });
+        strhtml += '</ul>';
+        $goodsList.html(strhtml);
+        // 2.懒加载
+        $(function () {
+          $("img.lazy").lazyload({ effect: "fadeIn" });
+        });
 
+        array_default = [];
+        array = [];
+        prev = null;
+        next = null;
+        $('.wrap ul>li').each(function (index, element) {
+          array[index] = $(this);
+          array_default[index] = $(this);
+        });
+      });
+      $('.page').pagination({
+        pageCount: 3,
+        jump: true,
+        prevContent: '上一页',
+        nextContent: '下一页',
+        callback: function (api) {
+          // console.log(api.getCurrent());
+          $.ajax({
+            url: 'http://localhost/homework-JS/youyiku/php/listdata.php',
+            data: {
+              page: api.getCurrent()
+            },
+            dataType: 'json'
+          }).done(function (data) {
+            let strhtml = '<ul>';
+            $.each(data, function (index, value) {
+              strhtml += `
+                <li>
+                  <div class="logo">
+                    <img src="${value.active}" alt="">
+                  </div>
+                  <div class="pic">
+                    <a href="details.html">
+                    <img data-original="${value.url}" class="lazy" width="212" height="222"/></a>
+                  </div>
+                  <ol>
+                    <li>
+                      <img src="${value.color1}" alt="">
+                    </li>
+                    <li>
+                      <img src="${value.color2}" alt="">
+                    </li>
+                    <li>
+                      <img src="${value.color3}" alt="">
+                    </li>
+                    <li>
+                      <img src="${value.color4}" alt="">
+                    </li>
+                    <li>
+                      <img src="${value.color5}" alt="">
+                    </li>
+                    <li>
+                      <img src="${value.color6}" alt="">
+                    </li>
+                  </ol>
+                  <p class="type">${value.title1}<span class="size">${value.size}</span></p>
+                  <p class="clothes">${value.title2}</p>
+                  <p class="price">${value.price}</p>
+                  <div class="star">
+                    <span class="iconfont icon-wujiaoxing"></span>
+                    <span class="iconfont icon-wujiaoxing"></span>
+                    <span class="iconfont icon-wujiaoxing"></span>
+                    <span class="iconfont icon-wujiaoxing"></span>
+                    <span class="iconfont icon-wujiaoxing"></span>
+                    <span>(${value.sailnumber})</span>
+                  </div>
+                </li>
+                `
+            });
+            strhtml += '</ul>';
+            $goodsList.html(strhtml);
+            // 2.懒加载
+            $(function () {
+              $("img.lazy").lazyload({ effect: "fadeIn" });
+            });
+            array_default = [];
+            array = [];
+            prev = null;
+            next = null;
+            $('.wrap ul>li').each(function (index, element) {
+              array[index] = $(this);
+              array_default[index] = $(this);
+            });
+          })
+        }
+      });
+      // 默认排序：
+      $btn.eq(0).on('click', function () {
+        $.each(array_default, function (index, value) {
+          $('.wrap>ul').append(value);
+        });
+        return;
+      });
+      // 升序：
+      $btn.eq(1).on('click', function () {
+        for (let i = 0; i < array.length - 1; i++) {
+          for (let j = 0; j < array.length - i - 1; j++) {
+            prev = parseFloat(array[j].find('.price').html().substring(1));
+            next = parseFloat(array[j + 1].find('.price').html().substring(1));
+            if (prev > next) {
+              let temp = array[j];
+              array[j] = array[j + 1];
+              array[j + 1] = temp;
+            }
+          }
+        }
+        $.each(array, function (index, value) {
+          $('.wrap>ul').append(value);
+        });
+      });
+      // 降序：
+      $btn.eq(2).on('click', function () {
+        for (let i = 0; i < array.length - 1; i++) {
+          for (let j = 0; j < array.length - i - 1; j++) {
+            prev = parseFloat(array[j].find('.price').html().substring(1));
+            next = parseFloat(array[j + 1].find('.price').html().substring(1));
+            if (prev < next) {
+              let temp = array[j];
+              array[j] = array[j + 1];
+              array[j + 1] = temp;
+            }
+          }
+        }
+        $.each(array, function (index, value) {
+          $('.wrap>ul').append(value);
+        });
+      })
     }
   }
 });
